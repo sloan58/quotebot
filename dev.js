0d2ce8cce374c9c5c069e5c2fd3bb624ff4919a1
sloan58 <marty@karmatek.io>
2021-01-11 18:26:55 -0500

    remove dev.js and do not track .vscode

--- dev.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-
-const quotes = JSON.parse(fs.readFileSync(__dirname + '/scripts/quotes.json'))
-const unique = [...new Set(quotes.map((item) => item.GENRE))]
-console.log(unique)
